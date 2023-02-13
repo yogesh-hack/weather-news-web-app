5e9587fe14b123aa77a1cd3543c884167c822ac6
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-02-13 19:15:18 +0530

    Add link

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='flex justify-center bottom-0 p-6 text-xl text-white rounded-xl'>
       
-        Made with ❤️ by 👤<a className='text-yellow-500 hover:text-red-500' href='/'><em>Yogesh Baghel </em> </a>
+        Made with ❤️ by 👤<a className='text-yellow-500 hover:text-red-500' href='https://github.com/yogesh-hack/'><em>Yogesh Baghel </em> </a>
         <br/>
             <span className=''> - @copyright 2023 - Weather News</span>
     </div>
